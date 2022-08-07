6cb93dbcdca49ca292d82b587652b5caeaa7e9d1
anatawa12 <anatawa12@icloud.com>
2022-08-07 22:54:22 +0900

    initial gui commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "openvr.h"
+#include <SDL.h>
+#include <GL/glew.h>
 
 #ifdef WIN32
 #include <windows.h>
@@ -8,6 +10,10 @@
 #include <csignal>
 #define Sleep(n) usleep(n * 1000)
 #endif
+
+#define WINDOW_CAPTION "clekeyOVR"
+#define WINDOW_HEIGHT 256
+#define WINDOW_WIDTH 512
 
 void handle_input_err(vr::EVRInputError error);
 
@@ -19,7 +25,83 @@ vr::VRActionHandle_t action_right_click;
 vr::VRActionHandle_t action_right_haptic;
 vr::VRActionSetHandle_t action_set_input;
 
-int main() {
+int main(int argc, char** argv) {
+    if (SDL_Init(SDL_INIT_VIDEO)) {
+        std::cerr << "sdl error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_Window * window = SDL_CreateWindow(
+            WINDOW_CAPTION,
+            0, 0,
+            WINDOW_WIDTH, WINDOW_HEIGHT,
+            SDL_WINDOW_OPENGL);
+    if (!window) {
+        std::cerr << "sdl error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+    if (!context) return -1;
+
+    // setup viewport
+    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    static const Uint32 interval = 1000 / 90;
+    static Uint32 nextTime = SDL_GetTicks() + interval;
+    while (true) {
+        // check event
+
+        SDL_Event ev;
+        SDL_Keycode key;
+        while ( SDL_PollEvent(&ev) )
+        {
+            switch(ev.type){
+                case SDL_QUIT:
+                    goto quit;
+                case SDL_KEYDOWN:
+                    key = ev.key.keysym.sym;
+                    if(key == SDLK_ESCAPE)
+                        goto quit;
+                    break;
+            }
+        }
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // draw sphere
+        glEnable(GL_COLOR_MATERIAL);
+        glColor3ub(255, 0, 0);
+        glBegin(GL_QUADS);
+#define POLY_SIZE .5
+
+        glVertex2d(-POLY_SIZE, -POLY_SIZE);
+        glVertex2d(+POLY_SIZE, -POLY_SIZE);
+        glVertex2d(+POLY_SIZE, +POLY_SIZE);
+        glVertex2d(-POLY_SIZE, +POLY_SIZE);
+
+        glVertex2d(-POLY_SIZE, -POLY_SIZE);
+        glVertex2d(-POLY_SIZE, +POLY_SIZE);
+        glVertex2d(+POLY_SIZE, +POLY_SIZE);
+        glVertex2d(+POLY_SIZE, -POLY_SIZE);
+
+        glEnd();
+
+        SDL_GL_SwapWindow(window);
+
+        int delayTime = (int) (nextTime - SDL_GetTicks());
+        if (delayTime > 0) {
+            SDL_Delay((Uint32) delayTime);
+        }
+
+        nextTime += interval;
+    }
+    quit:
+    SDL_Quit();
+
+#if 0
     std::cout << "Hello, World!" << std::endl;
 
     vr::HmdError err;
@@ -75,6 +157,7 @@ int main() {
     std::cout << "shutdown finished" << std::endl;
 
     return 0;
+#endif
 }
 
 void handle_input_err(vr::EVRInputError error) {
