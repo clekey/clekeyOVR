0bf998d3ae56edb88d3ef71afaf8c6a27e889a32
anatawa12 <anatawa12@icloud.com>
2022-08-08 19:11:45 +0900

    upgrade opengl to 3.2

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "openvr.h"
 #include <SDL.h>
 #include <GL/glew.h>
+#include <vector>
 
 #ifdef WIN32
 #include <windows.h>
@@ -15,6 +16,8 @@
 #define WINDOW_HEIGHT 256
 #define WINDOW_WIDTH 512
 
+#define check_gl_err() check_gl_err_impl(__LINE__)
+void check_gl_err_impl(int line);
 void handle_input_err(vr::EVRInputError error);
 
 vr::VRActionHandle_t action_left_stick;
@@ -42,12 +45,105 @@ int main(int argc, char** argv) {
         return -1;
     }
 
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
     SDL_GLContext context = SDL_GL_CreateContext(window);
     if (!context) return -1;
+
+    glewExperimental = true;
+    glewInit();
+
+    GLuint vertex_array;
+    glGenVertexArrays(1, &vertex_array);
+    glBindVertexArray(vertex_array);
+
+    GLuint shader_program;
+    {
+        GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+        GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+        const char *vertex_shader_src =
+                "#version 330 core\n"
+                "layout(location = 0) in vec3 vertexPosition_modelspace;\n"
+                "void main() {\n"
+                "    gl_Position.xyz = vertexPosition_modelspace;\n"
+                "}\n";
+        const char *fragment_shader_src =
+                "#version 330 core\n"
+                "// Ouput data\n"
+                "out vec3 color;\n"
+                "\n"
+                "void main() {\n"
+                "    // Output color = red \n"
+                "    color = vec3(1,0,0);\n"
+                "}\n";
+
+        glShaderSource(vertex_shader, 1, &vertex_shader_src, nullptr);
+        glCompileShader(vertex_shader);
+
+        {
+            GLint result;
+            GLint info_log_len;
+
+            glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &result);
+            glGetShaderiv(vertex_shader, GL_INFO_LOG_LENGTH, &info_log_len);
+            if (info_log_len != 0) {
+                std::vector<char> shader_err_msg(info_log_len);
+                glGetShaderInfoLog(vertex_shader, info_log_len, nullptr, &shader_err_msg[0]);
+                fprintf(stdout, "%s\n", &shader_err_msg[0]);
+            }
+        }
+
+        glShaderSource(fragment_shader, 1, &fragment_shader_src, nullptr);
+        glCompileShader(fragment_shader);
+
+        {
+            GLint result;
+            GLint info_log_len;
+
+            glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &result);
+            glGetShaderiv(fragment_shader, GL_INFO_LOG_LENGTH, &info_log_len);
+            if (info_log_len != 0) {
+                std::vector<char> shader_err_msg(info_log_len);
+                glGetShaderInfoLog(fragment_shader, info_log_len, nullptr, &shader_err_msg[0]);
+                fprintf(stdout, "%s\n", &shader_err_msg[0]);
+            }
+        }
+
+        shader_program = glCreateProgram();
+        glAttachShader(shader_program, vertex_shader);
+        glAttachShader(shader_program, fragment_shader);
+        glLinkProgram(shader_program);
+
+        {
+            GLint result;
+            GLint info_log_len;
+
+            glGetProgramiv(shader_program, GL_LINK_STATUS, &result);
+            glGetProgramiv(shader_program, GL_INFO_LOG_LENGTH, &info_log_len);
+            if (info_log_len != 0) {
+                std::vector<char> shader_err_msg(info_log_len);
+                glGetShaderInfoLog(shader_program, info_log_len, nullptr, &shader_err_msg[0]);
+                fprintf(stdout, "%s\n", &shader_err_msg[0]);
+            }
+        }
+
+        glDeleteShader(vertex_shader);
+        glDeleteShader(fragment_shader);
+    }
 
     // setup viewport
     glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    static const GLfloat g_vertex_buffer_data[] = {
+            -1.0f, -1.0f, 0.0f,
+            1.0f, -1.0f, 0.0f,
+            0.0f,  1.0f, 0.0f,
+    };
+    GLuint vertexbuffer;
+    glGenBuffers(1, &vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
     static const Uint32 interval = 1000 / 90;
     static Uint32 nextTime = SDL_GetTicks() + interval;
@@ -113,29 +209,17 @@ int main(int argc, char** argv) {
             }
         }
 
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-        glTranslatef(-1, 1, 0);
-        glScaled(2.0 / WINDOW_WIDTH, -2.0 / WINDOW_HEIGHT, 1);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(shader_program);
 
-        // draw sphere
-        glEnable(GL_COLOR_MATERIAL);
-        glColor3ub(255, 0, 0);
-        glBegin(GL_QUADS);
-#define POLY_SIZE 25
+        // 1rst attribute buffer : vertices
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+        // Draw the triangle !
+        glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
+        glDisableVertexAttribArray(0);
 
-        glVertex2d(-POLY_SIZE, -POLY_SIZE);
-        glVertex2d(+POLY_SIZE, -POLY_SIZE);
-        glVertex2d(+POLY_SIZE, +POLY_SIZE);
-        glVertex2d(-POLY_SIZE, +POLY_SIZE);
-
-        glVertex2d(-POLY_SIZE + 100, -POLY_SIZE + 100);
-        glVertex2d(+POLY_SIZE + 100, -POLY_SIZE + 100);
-        glVertex2d(+POLY_SIZE + 100, +POLY_SIZE + 100);
-        glVertex2d(-POLY_SIZE + 100, +POLY_SIZE + 100);
-
-        glEnd();
+        check_gl_err();
 
         SDL_GL_SwapWindow(window);
 
@@ -148,12 +232,23 @@ int main(int argc, char** argv) {
     }
 
     quit:
+    // Cleanup VBO
+    glDeleteBuffers(1, &vertexbuffer);
+    glDeleteVertexArrays(1, &vertex_array);
+    glDeleteProgram(shader_program);
     SDL_Quit();
     vr::VR_Shutdown();
 
     std::cout << "shutdown finished" << std::endl;
 
     return 0;
+}
+
+void check_gl_err_impl(int line) {
+    GLenum err;
+    while (err = glGetError()) {
+        std::cerr << "err #" << line << ": " << gluErrorString(err) << std::endl;
+    }
 }
 
 void handle_input_err(vr::EVRInputError error) {
