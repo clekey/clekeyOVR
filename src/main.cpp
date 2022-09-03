c4e74d4d8d2dc54716373e781f9bda572f37fc95
anatawa12 <anatawa12@icloud.com>
2022-09-03 15:37:56 +0900

    higher resolution

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,8 @@
 #include "graphics/bmp_export.h"
 
 #define WINDOW_CAPTION "clekeyOVR"
-#define WINDOW_HEIGHT 256
-#define WINDOW_WIDTH 512
+#define WINDOW_HEIGHT 512
+#define WINDOW_WIDTH 1024
 
 // error handling
 
