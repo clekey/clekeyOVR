1a4c070a81e9b86db1925b1d42943b8250f1a375
anatawa12 <anatawa12@icloud.com>
2022-09-04 16:00:30 +0900

    add test renderer

--- src/freetype_test.cpp
+++ src/freetype_test.cpp
@@ -126,13 +126,13 @@ void export_as_bmp(uint32_t width, uint32_t height, uint8_t data[]) {
 class F26Dot6 {
 public:
   FT_F26Dot6 value;
+
   explicit F26Dot6(FT_F26Dot6 value) : value(value) {}
 };
 
-std::ostream& operator<<(std::ostream& os, const F26Dot6& value)
-{
+std::ostream &operator<<(std::ostream &os, const F26Dot6 &value) {
   os << (value.value >> 6) << '.';
-  int rest = (int)(value.value & 0x3F);
+  int rest = (int) (value.value & 0x3F);
   do {
     rest *= 10;
     os << (rest >> 6);
@@ -140,6 +140,82 @@ std::ostream& operator<<(std::ostream& os, const F26Dot6& value)
   } while (rest != 0);
   return os;
 }
+
+class Renderer {
+  freetype::Face face;
+  int cursor;
+public:
+  int baseline;
+  int width;
+  int height;
+  std::vector<uint8_t> data;
+
+  Renderer(freetype::Face face, int width, int height) : face(std::move(face)), width(width), height(height),
+                                                         data(height * width) {
+    cursor = 0;
+    baseline = height / 2;
+
+    for (int x = 0; x < width; ++x) {
+      draw_dot(x, baseline, 127);
+    }
+  }
+
+  void draw_dot(int x, int y, uint8_t color) {
+    if (color == 255) {
+      data[y * width + x] = 255;
+      return; // fast path
+    } else if (color == 0) {
+      return; // fast path
+    } else {
+      float dst = ((float) data[y * width + x]) / 255.0f;
+      float src = ((float) color) / 255.0f;
+      data[y * width + x] = (int) ((dst * (1 - src) + src) * 255);
+    }
+  }
+
+  void draw_string(const char32_t *c) {
+    while (*c != U'\0') {
+      draw_char(*c++);
+    }
+  }
+
+  void draw_char(char32_t c) {
+    face.setPixelSizes(baseline, 0);
+    face.loadChar(c, FT_LOAD_RENDER);
+    auto glyph = face->glyph;
+    const auto pitchAbs = abs(glyph->bitmap.pitch);
+
+    int xMin = (int) (glyph->metrics.horiBearingX >> 6);
+    int yMin = (int) ((glyph->metrics.horiBearingY >> 6) - (glyph->metrics.height >> 6));
+    int xMinPos = xMin + cursor;
+    int yMinPos = yMin + baseline;
+
+    if (glyph->bitmap.pitch < 0) {
+      // glyph->bitmap.buffer is down to top: keep y
+      for (int fontY = 0, dataY = yMinPos; fontY < glyph->bitmap.rows; ++fontY, ++dataY) {
+        for (int fontX = 0, dataX = xMinPos; fontX < glyph->bitmap.width; ++fontX, ++dataX) {
+          draw_dot(dataX, dataY, glyph->bitmap.buffer[fontY * pitchAbs + fontX]);
+        }
+      }
+    } else {
+      // glyph->bitmap.buffer is top to down: inverse y
+      for (int fontY = (int) (glyph->bitmap.rows - 1), dataY = yMinPos; fontY >= 0; --fontY, ++dataY) {
+        for (int fontX = 0, dataX = xMinPos; fontX < glyph->bitmap.width; ++fontX, ++dataX) {
+          draw_dot(dataX, dataY, glyph->bitmap.buffer[fontY * pitchAbs + fontX]);
+        }
+      }
+    }
+
+    cursor += (int) (glyph->metrics.horiAdvance >> 6);
+    // addtional: show origin point
+
+    draw_dot(cursor - 1, baseline, 255);
+    draw_dot(cursor + 1, baseline, 255);
+    draw_dot(cursor, baseline - 1, 255);
+    draw_dot(cursor, baseline + 1, 255);
+    draw_dot(cursor, baseline, 255);
+  }
+};
 
 int main(int argc, char **argv) {
   freetype::Freetype ft;
@@ -167,8 +243,7 @@ int main(int argc, char **argv) {
     }
   }
 
-  export_as_bmp(glyph->bitmap.width, glyph->bitmap.rows, &bmp[0]);
-
+  //export_as_bmp(glyph->bitmap.width, glyph->bitmap.rows, &bmp[0]);
 
   std::cout << "units_per_EM: " << face->units_per_EM << std::endl;
   std::cout << "ascender:     " << face->ascender * char_size / face->units_per_EM << std::endl;
@@ -186,6 +261,14 @@ int main(int argc, char **argv) {
   std::cout << "vertBearingX: " << (F26Dot6(glyph->metrics.vertBearingX)) << std::endl;
   std::cout << "vertBearingY: " << (F26Dot6(glyph->metrics.vertBearingY)) << std::endl;
   std::cout << "vertAdvance:  " << (F26Dot6(glyph->metrics.vertAdvance)) << std::endl;
+
+  Renderer renderer(std::move(face), 1024 * 3, 128);
+  renderer.draw_string(U"ABC あいうえお 漢字 αβγ '　'<=全角sp "
+                       U"か" U"\u3099"
+                       U"ほ" U"\u309A"
+                       U"a" "\u0308"
+                       U"<=結合文字(がぽä)");
+  export_as_bmp(renderer.width, renderer.height, &renderer.data[0]);
 
   //glyph->advance;
   //metrics.horiBearingX
