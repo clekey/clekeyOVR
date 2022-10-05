5bab60229d1532b8e69aba97d71a30579c3a3e58
anatawa12 <anatawa12@icloud.com>
2022-10-06 07:55:06 +0900

    implement global.rs

--- src/global.rs
+++ src/global.rs
@@ -1,5 +1,23 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use once_cell::sync::Lazy;
 
-pub fn get_config_dir() -> PathBuf {
+pub fn get_exe_dir() -> PathBuf {
+    std::env::current_exe()
+        .expect("failed to get current exe path")
+        .parent()
+        .expect("current exe is not in a folder")
+        .to_owned()
+}
 
+pub fn get_resources_dir() -> PathBuf {
+    get_exe_dir().join("resources")
+}
+
+#[cfg(windows)]
+pub fn get_config_dir() -> &'static Path {
+    static VALUE: Lazy<PathBuf> = Lazy::new(|| {
+        PathBuf::from(std::env::var_os("APPDATA").expect("no APPDATA found"))
+            .join("clekey_ovr")
+    });
+    return &*VALUE;
 }
