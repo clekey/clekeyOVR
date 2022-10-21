3c2c9770f774ba0b4699d9dc55e2e25f200cb879
anatawa12 <anatawa12@icloud.com>
2022-10-21 22:08:29 +0900

    wip: fix something

--- build.rs
+++ build.rs
@@ -4,7 +4,4 @@ fn main() {
     if cfg!(debug_assertions) && cfg!(feature = "default") {
         println!(r#"cargo:rustc-cfg=feature="debug_window""#);
     }
-    if var("CARGO_CFG_TARGET_OS").as_deref() == Ok("windows") && cfg!(feature = "default") {
-        println!(r#"cargo:rustc-cfg=feature="openvr""#);
-    }
 }
